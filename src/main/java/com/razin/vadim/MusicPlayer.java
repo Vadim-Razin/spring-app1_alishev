1050d339da0c45565eb0f32f962eac030b1eb334
Vadim Razin <vadim.razin@gmail.com>
2022-12-14 06:33:58 +0600

    Finsh lesson 12 Аннотация  @Qualifier. Внедрение зависимостей (Dependency Injection) w/o home work

--- src/main/java/com/razin/vadim/MusicPlayer.java
+++ src/main/java/com/razin/vadim/MusicPlayer.java
@@ -1,48 +1,21 @@
 package com.razin.vadim;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MusicPlayer {
-    private Music music;
-    private String name;
-    private int volume;
-    private List<Music> musicList = new ArrayList<>();
 
-    public MusicPlayer() {
+    private Music music1;
+    private Music music2;
+    @Autowired
+    public MusicPlayer(@Qualifier("rockMusic") Music music1, @Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
-    public void setMusicList(List<Music> musicList) {
-        this.musicList = musicList;
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public void playMusic() {
-
-        System.out.println("Playing: " + music.getSong());
-
+    public String playMusic() {
+        return "Playing: " + music1.getSong() + " " + music2.getSong();
     }
 }
